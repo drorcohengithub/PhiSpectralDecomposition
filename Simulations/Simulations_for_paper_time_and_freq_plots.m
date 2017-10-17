152cd3a71d1084f3d386fc30a478d26b2bb4ad91
DrorGitHub <dror.cohen07@gmail.com>
2017-10-17 17:21:35 +1100

    Added eigenval decomp

--- Simulations/Simulations_for_paper_time_and_freq_plots.m
+++ Simulations/Simulations_for_paper_time_and_freq_plots.m
@@ -140,8 +140,7 @@ if systemIndx<3
     plot(freqs,sdecomp_multi_info,'g');
     plot(freqs,sdecomp_Phi_G,'k:','linewidth',2)
     legend({'GC x_2 to x_1','GC x_1 to x_2','multi. info.','Phi G'},'fontsize',20,'linewidth',2)
-    xlabel('Frequency')
-    set(gca,'fontsize',20)
+ 
 else
     hold on
     hl=plot(freqs,squeeze(spct_GC(1,2,:)),'r',freqs,squeeze(spct_GC(2,1,:)),'r--');
@@ -151,31 +150,44 @@ else
     plot(freqs,sdecomp_multi_info,'g');
     plot(freqs,sdecomp_Phi_G,'k:','linewidth',2)
     legend({'GC x_2 to x_1','GC x_1 to x_2','sum of GCs','multi. info.','Phi G'},'fontsize',20,'linewidth',2)
-    xlabel('Frequency')
-    set(gca,'fontsize',20)
 %     plot(freqs,squeeze(real(H(2,1,:))));
 
 end
 
+xlabel('Frequency')
+set(gca,'fontsize',20)
+set(gca,'xticklabel',[])
 
 %% save
 svFldr = ['/Users/dror/Google Drive/Sasai/FIGURES/Simulations/' systems(systemIndx).nm];
 saveFigAs(svFldr,0,'halfScreen')
 
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% The code below verfies some analytical relationships presented in the paper. 
+%% Examine eigenvalue decomposition
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% Check: Relationshup between spectral decomp of multi info and coherence
-C=squeeze((abs(S(1,2,:)).^2)./(S(1,1,:).*S(2,2,:)));
-%mutual info rate
-sdecomp_multi_info_2 = -log(1-C);
-clf
-plot(freqs,sdecomp_multi_info-sdecomp_multi_info_2); ylim(1e-10*[-1 1]);%  note the scale
+% Breakdown the determinants of the reduced models to their eigenvalues
+    
+% full system
+eigvals_S = [];
 
-%% Check: Get GC through through reducde model framework
-% do this by enforce off-diagonal to zero for GC
+% Phi G
+eigvals_S_r = [];
+
+% GC
+eigvals_S_r_GC = [];
+
+% multi info (eigenvalues are just the power spcts)
+eigvals_multiinfo = [];
+
+S_multiinfo = S;
+
+S_multiinfo(1,2,:) = 0;
+S_multiinfo(2,1,:) = 0;
+
+%% also look at GC
+% to do this by enforce off-diagonal to zero for GC
 split_mask= ones(N);
 split_mask(2,1)=0;
     
@@ -185,51 +197,192 @@ gamma = 0.01;
 min_error = 1e-6;
 [S_r_GC,det_S_r_GC,trace_S_r_GC,prod_diag_S_r_GC,A_r_GC,SIG_r_GC,masked_Delta_GC] =...
     get_reduced_S_from_autoCov(G,split_mask,max_order,freq_res,iter_max,gamma,min_error);
+%% get the eigen valus
+
+for freqIndx=1:size(S,3)
     
-%compare with MVGC
-GC_x1_to_x2_2=log( det (SIG_r_GC) / det (SIG_f) );
-%time domain
-disp(GC_x1_to_x2-GC_x1_to_x2_2);
+    [ignr tmp] = eig(squeeze(S(:,:,freqIndx)));
+    eigvals_S(freqIndx,:)=diag(tmp);
+    
+    [ignr tmp] = eig(squeeze(S_r_GC(:,:,freqIndx)));
+    eigvals_S_r_GC(freqIndx,:)=diag(tmp); 
+    
+    [ignr tmp] = eig(squeeze(S_r(:,:,freqIndx)));
+    eigvals_S_r(freqIndx,:)=diag(tmp);
+    
+    [ignr tmp] = eig(squeeze(S_multiinfo(:,:,freqIndx)));
+    eigvals_multiinfo(freqIndx,:)=diag(tmp);
+    
+    
+end
 
-%freq domain
-sdecomp_GC_x1_to_x2_2=log(det_S_r_GC./det_S');
-plot(freqs,real(sdecomp_GC_x1_to_x2_2)-sdecomp_GC_x1_to_x2) % this can be reduced by decreasing the min_error arg
+%% order the eigen values
+eigvals_S = log(real(sort(eigvals_S,2,'descend')));
+eigvals_S_r_GC = log(real(sort(eigvals_S_r_GC,2,'descend')));
+eigvals_S_r = log(real(sort(eigvals_S_r,2,'descend')));
+eigvals_multiinfo = log(real(sort(eigvals_multiinfo,2,'descend')));
 
-%check summation across freqs
-GC_x1_to_x2_3 = mean([sdecomp_GC_x1_to_x2_2;sdecomp_GC_x1_to_x2_2(2:freq_res)]);
-disp(real(GC_x1_to_x2_3 - GC_x1_to_x2_2)) % this can be reduced by decreasing the min_error arg
-%% other dirn also works, just replace:
-%split_mask= ones(N);
-%split_mask(2,1)=0;
-% With
-%split_mask= ones(N);
-%split_mask(1,2)=0;
-   
-%% Check: Using power spectrums to estimate GC.
-% Typically, to estimate GC we would have to fit a reduced model. However
-% it turns out that we can simply use the power spectrum to estimate the 
-% variance 
+%% normalize by det_S
+eigvals_S_r_GC_norm = eigvals_S_r_GC - eigvals_S;
+eigvals_S_r_norm = eigvals_S_r - eigvals_S;
+eigvals_multiinfo_norm = eigvals_multiinfo - eigvals_S;
 
-% variance of the individual models based on the power spct (two sided)
-two_sided_S = cat(3,S,S(:,:,2:freq_res));
-% We don't care about these here
-two_sided_S(1,2,:) = 0;
-two_sided_S(2,1,:) = 0;
+%store here
+eig_cell={};
+eig_cell{1,1} = eigvals_S_r_GC;
+eig_cell{1,2} = eigvals_S_r_GC_norm;
 
-% sum of log 
-tmpS = squeeze(mean(log(two_sided_S),3));
-log_E22_1 = tmpS(2,2); 
-log_E11_1 = tmpS(1,1);
-% compare with log_E22 and log_E11 as estimated above
-disp(log_E22_1 - log_E22);
-disp(log_E11 - log_E11);
+eig_cell{2,1} = eigvals_S_r;
+eig_cell{2,2} = eigvals_S_r_norm;
 
-%% Check: correspondance between time and freq domain versions of multi info and Phi G
-% Info geom def of multi info
-sbased_multi_info = mean([sdecomp_multi_info; sdecomp_multi_info(2:freq_res)]);
-disp(sbased_multi_info - multi_info)
+eig_cell{3,1} = eigvals_multiinfo;
+eig_cell{3,2} = eigvals_multiinfo_norm;
 
-sbased_Phi_G = mean([sdecomp_Phi_G; sdecomp_Phi_G(2:freq_res)]);
-disp(sbased_Phi_G - Phi_G)
+reduced_S_nms = {'S_r_GC','S_r_Phi','S_r_multiinfo'};
 
+%% plot properties for reduced sys 
+
+clf
+set(0,'DefaultAxesFontSize',20)
+set(0,'DefaultLineLinewidth',2)
+% set(0,'DefaultAxesXlim','tight')
+
+j=1;
+ax(j) = subplot(2,3,j); j = j+1;
+hold on
+plot(freqs,eigvals_S(:,1),...
+    freqs,eigvals_S_r_GC(:,1),'r',...
+    freqs,eigvals_S_r(:,1),'b',...
+    freqs,eigvals_multiinfo(:,1),'g');
+
+ylim([-5 5])
+title('log(Eig 1)')
+
+ax(j) = subplot(2,3,j); j = j+1;
+plot(freqs,eigvals_S(:,2),...
+    freqs,eigvals_S_r_GC(:,2),'r',...
+    freqs,eigvals_S_r(:,2),'b',...
+    freqs,eigvals_multiinfo(:,2),'g');
+
+ylim([-5 5])
+title('log(Eig 2)')
+
+ax(j) = subplot(2,3,j); j = j+1;
+plot(freqs,sum(eigvals_S,2),...
+    freqs,sum(eigvals_S_r_GC,2),'r',...
+    freqs,sum(eigvals_S_r,2),'b',...
+    freqs,sum(eigvals_multiinfo,2),'g');
+
+ylim([-5 5])
+title('log(Eig 1) + log(Eig 1)')
+
+% diff
+ax(j) = subplot(2,3,j); j = j+1;
+hold on
+plot(freqs,eigvals_S_r_GC_norm(:,1),'r',...
+    freqs,eigvals_S_r_norm(:,1),'b',...
+    freqs,eigvals_multiinfo_norm(:,1),'g');
+
+title('\Deltalog(Eig 1)')
+xlabel('Frequency');
+ylim([-1 1])
+
+
+ax(j) = subplot(2,3,j); j = j+1;
+plot(freqs,eigvals_S_r_GC_norm(:,2),'r',...
+    freqs,eigvals_S_r_norm(:,2),'b',...
+    freqs,eigvals_multiinfo_norm(:,2),'g');
+
+title('\Deltalog(Eig 2)')
+xlabel('Frequency');
+ylim([-1 1])
+
+
+
+ax(j) = subplot(2,3,j); j = j+1;
+plot(freqs,sum(eigvals_S_r_GC_norm,2),'r',...
+    freqs,sum(eigvals_S_r_norm,2),'b',...
+    freqs,sum(eigvals_multiinfo_norm,2),'g');
+
+title('\Deltalog(Eig 1)+\Deltalog(Eig 2)')
+xlabel('Frequency');
+ylim([-1 1])
+
+  
+suptitle(reduced_S_nms{reduced_S_indx});
+
+set(ax,'xlim',[0 500],'xticklabel',[])
+
+    % ,freqs,eigvals_S_norm(:,2))
+    % plot(freqs,log(eigvals_S_r(:,1)),freqs,log(eigvals_S_r(:,2)),freqs,sum(log(eigvals_S_r),2),freqs,log(det_S'))
+    
+%     set(ax,'Nextplot','add')
+%     hold(ax,'on')
+    % save
+    svFldr = ['/Users/dror/Google Drive/Sasai/FIGURES/Simulations/' systems(systemIndx).nm '_Eigdecomp_' reduced_S_nms{reduced_S_indx}];
+    saveFigAs(svFldr,0,'halfScreen')
+
+    
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% The code below verfies some analytical relationships presented in the paper. 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if 0
+    %% Check: Relationshup between spectral decomp of multi info and coherence
+    C=squeeze((abs(S(1,2,:)).^2)./(S(1,1,:).*S(2,2,:)));
+    %mutual info rate
+    sdecomp_multi_info_2 = -log(1-C);
+    clf
+    plot(freqs,sdecomp_multi_info-sdecomp_multi_info_2); ylim(1e-10*[-1 1]);%  note the scale
+
+    %% Check: Get GC through through reducde model framework
+
+    %compare with MVGC
+    GC_x1_to_x2_2=log( det (SIG_r_GC) / det (SIG_f) );
+    %time domain
+    disp(GC_x1_to_x2-GC_x1_to_x2_2);
+
+    %freq domain
+    sdecomp_GC_x1_to_x2_2=log(det_S_r_GC./det_S');
+    plot(freqs,real(sdecomp_GC_x1_to_x2_2)-sdecomp_GC_x1_to_x2) % this can be reduced by decreasing the min_error arg
+
+    %check summation across freqs
+    GC_x1_to_x2_3 = mean([sdecomp_GC_x1_to_x2_2;sdecomp_GC_x1_to_x2_2(2:freq_res)]);
+    disp(real(GC_x1_to_x2_3 - GC_x1_to_x2_2)) % this can be reduced by decreasing the min_error arg
+    %% other dirn also works, just replace:
+    %split_mask= ones(N);
+    %split_mask(2,1)=0;
+    % With
+    %split_mask= ones(N);
+    %split_mask(1,2)=0;
+
+    %% Check: Using power spectrums to estimate GC.
+    % Typically, to estimate GC we would have to fit a reduced model. However
+    % it turns out that we can simply use the power spectrum to estimate the 
+    % variance 
+
+    % variance of the individual models based on the power spct (two sided)
+    two_sided_S = cat(3,S,S(:,:,2:freq_res));
+    % We don't care about these here
+    two_sided_S(1,2,:) = 0;
+    two_sided_S(2,1,:) = 0;
+
+    % sum of log 
+    tmpS = squeeze(mean(log(two_sided_S),3));
+    log_E22_1 = tmpS(2,2); 
+    log_E11_1 = tmpS(1,1);
+    % compare with log_E22 and log_E11 as estimated above
+    disp(log_E22_1 - log_E22);
+    disp(log_E11 - log_E11);
+
+    %% Check: correspondance between time and freq domain versions of multi info and Phi G
+    % Info geom def of multi info
+    sbased_multi_info = mean([sdecomp_multi_info; sdecomp_multi_info(2:freq_res)]);
+    disp(sbased_multi_info - multi_info)
+
+    sbased_Phi_G = mean([sdecomp_Phi_G; sdecomp_Phi_G(2:freq_res)]);
+    disp(sbased_Phi_G - Phi_G)
+end
 
