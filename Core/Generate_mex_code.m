4298331ed7d225ebc500b3ab585d951c61f60f54
dc <dror.cohen07@gmail.com>
2018-12-21 13:20:06 +0900

    some mex code stuff

--- Core/Generate_mex_code.m
+++ Core/Generate_mex_code.m
@@ -1,4 +1,5 @@
 %This script generates the mex scripts to accelrate the computations
+% **This requires the matlab MATLAB Coder toolbox**
 
 %check mvgc is on path
 onPath = ~isempty(strfind(path, 'mvgc'));
@@ -10,14 +11,15 @@ end
 % where is adjmat_argminGV_mask
 path_of_script = mfilename('fullpath');
 
-% fine last slash
+% find last slash
 slashes = strfind(path_of_script,'/');
 last_slash = slashes(end);
-path_of_script = [path_of_script(1:last_slash) '/mex_stuff/'];
+path_to_func = path_of_script(1:last_slash);
+out_dir_script = [path_to_func '/mex_stuff/'];
 
-% path_of_script = path_of_script
-eval(['codegen adjmat_argminGV_mask.m -d ' path_of_script])
+%
+eval(['codegen adjmat_argminGV_mask.m -d ' out_dir_script])
 
-% make sure noew mex stuff is on path
+% make sure new mex stuff is on path
 addpath(genpath(path_of_script))
 
